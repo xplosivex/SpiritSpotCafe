1c711127e8067f5b6ac8e1d00614b2c99bec4f66
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-20 12:00:59 -0700

    Create Option.js

---
+++ Scripts/Option.js
@@ -0,0 +1,10 @@
+function show(aval) {
+    if (aval == "Hot Chocolate" || aval == "Hot Caramel"  || aval == "Hot Vanilla" || aval == "Iced Caramel" || aval == "Iced Vanilla"  ) {
+    hiddenDiv.style.display='block';
+     hiddenDiv.style.float='left';
+    Form.fileURL.focus();
+    } 
+    else{
+    hiddenDiv.style.display='none';
+    }
+  }
