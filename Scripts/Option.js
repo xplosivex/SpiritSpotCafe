f02eafe0cf359ef6a8f50d97b0b7a4b988c9eac1
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-20 12:06:40 -0700

    Update Option.js

--- Scripts/Option.js
+++ Scripts/Option.js
@@ -1,7 +1,7 @@
 function show(aval) {
     if (aval == "Hot Chocolate" || aval == "Hot Caramel"  || aval == "Hot Vanilla" || aval == "Iced Caramel" || aval == "Iced Vanilla"  ) {
     hiddenDiv.style.display='block';
-     hiddenDiv.style.float='left';
+    
     Form.fileURL.focus();
     } 
     else{
