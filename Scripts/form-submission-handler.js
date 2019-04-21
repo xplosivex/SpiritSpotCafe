c055f065666072c91495ba22b8d640fd5fabe706
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-21 10:20:58 -0700

    Update and rename Form.js to form-submission-handler.js

--- Scripts/form-submission-handler.js
+++ Scripts/form-submission-handler.js
@@ -91,7 +91,7 @@
           if (formElements) {
             formElements.style.display = "none"; // hide form
           }
-      var thankYouMessage = form.querySelector(".thankyou_message");
+          var thankYouMessage = form.querySelector(".thankyou_message");
           if (thankYouMessage) {
             thankYouMessage.style.display = "block";
           }
