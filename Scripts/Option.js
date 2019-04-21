aeb9f1ec54465d39bdd1b2d290c023c0f666d03d
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-21 12:02:39 -0700

    Update Option.js

--- Scripts/Option.js
+++ Scripts/Option.js
@@ -1,25 +1,13 @@
 function show(aval) {
     if ( aval == "Hot Caramel"  || aval == "Hot Vanilla" || aval == "Iced Caramel" || aval == "Iced Vanilla"  ) {
-    hiddenDiv.style.display='block';
-    
-    Form.fileURL.focus();
-    } 
-    else{
-    hiddenDiv.style.display='none';
-        document.getElementById("Toppings").value = "None";
-        
-    }
-  }
-
-function show(aval) {
-    if ( aval == "Yes" ) {
     hiddenDiv2.style.display='block';
     
     Form.fileURL.focus();
     } 
     else{
     hiddenDiv2.style.display='none';
-     
+        document.getElementById("Toppings").value = "None";
         
     }
   }
+
