fd481e08d6143dd718334dd5534fa6f3fdd22ec4
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-21 12:20:07 -0700

    Update Option.js

--- Scripts/Option.js
+++ Scripts/Option.js
@@ -9,13 +9,15 @@ var here = document.getElementById("Type").value
     hiddenDiv.style.display='none';
 document.getElementById("Toppings").value = "None";
 }
+}
+function change2(){
     var here2 = document.getElementById("Extra").value 
  if ( here2 == "Yes") {
     hiddenDiv2.style.display='block';
     
     Form.fileURL.focus();
     } 
-    else{
+    else if ( here2 == "No") {
     hiddenDiv2.style.display='none';
 
 }
