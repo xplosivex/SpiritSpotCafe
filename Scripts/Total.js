66b52c725157b14fc8372c9528e705450e9d2ccc
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-22 14:57:44 -0700

    Update Total.js

--- Scripts/Total.js
+++ Scripts/Total.js
@@ -340,7 +340,10 @@ switch(IIQ) {
     break;
 }
     var ExtraS = AAQ + " " + BBQ + " " + CCQ + " " + DDQ + " " + EEQ + " " + FFQ + " " + GGQ + " " + HHQ + " " + IIQ + " " + JJQ + " " + KKQ + " " + LLQ + " " + MMQ + " " + NNQ + " " OOQ + " " + PPQ + " " + QQQ + " " + RRQ + " " + SSQ + " " + TTQ
-     var TotalK =  AT +  AE + BE + CE + DE + EE + FE + GE + HE + IE + JE + KE + LE + ME + NE + OE + PE + QE + RE + SE + TE
+   
+var ExtrasS = ExtraS.replace('0','');
+    document.getElementById("ExtraSelect").innerHTML = ExtrasS;
+    var TotalK =  AT +  AE + BE + CE + DE + EE + FE + GE + HE + IE + JE + KE + LE + ME + NE + OE + PE + QE + RE + SE + TE
     var TotalT =  "Total: " + "$" + TotalK
     if (FFF == "NDD") {
     var TotalG =  "Total: " + "$" + TotalK + " + Special Request"
