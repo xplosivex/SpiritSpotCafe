481bdc754c815b1891e175906120a1033222ec9f
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-22 15:11:08 -0700

    Update Total.js

--- Scripts/Total.js
+++ Scripts/Total.js
@@ -342,7 +342,7 @@ switch(IIQ) {
     var ExtraS = AAQ + " " + BBQ + " " + CCQ + " " + DDQ + " " + EEQ + " " + FFQ + " " + GGQ + " " + HHQ + " " + IIQ + " " + JJQ + " " + KKQ + " " + LLQ + " " + MMQ + " " + NNQ + " " + OOQ + " " + PPQ + " " + QQQ + " " + RRQ + " " + SSQ + " " + TTQ
    
  
-    document.getElementById("ExtraSelect").value = ExtraS.replace(' 0 ','');
+    document.getElementById("ExtraSelect").value = ExtraS.replace(/0/g, '');;
     var TotalK =  AT +  AE + BE + CE + DE + EE + FE + GE + HE + IE + JE + KE + LE + ME + NE + OE + PE + QE + RE + SE + TE
     var TotalT =  "Total: " + "$" + TotalK
     if (FFF == "NDD") {
