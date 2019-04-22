6350a753cd600c6b2763a03cee23414a7474270b
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-22 09:40:36 -0700

    Update Total.js

--- Scripts/Total.js
+++ Scripts/Total.js
@@ -1,4 +1,5 @@
-setInterval(Total,0) 
+ setInterval(Total,0)
+
    function Total(){
  var A =  document.getElementById("ExtraA").checked 
  var B = document.getElementById("ExtraB").checked
@@ -20,105 +21,108 @@ var Q = document.getElementById("ExtraQ").checked
 var R = document.getElementById("ExtraR").checked
 var S = document.getElementById("ExtraS").checked
 var T = document.getElementById("ExtraT").checked
-if (A == "true") {
-var AE = "1"   
+if (A == true) {
+var AE = 1   
 }else{
-var AE = "0"
+var AE = 0
 }
-if (B == "true") {
-var BE = "2"   
+if (B == true) {
+var BE = 2  
 }else{
-var BE = "0"
+var BE = 0
 }
-if (C == "true") {
-var CE = "1"  
+if (C == true) {
+var CE = 1  
 }else{
-var CE = "0"
+var CE = 0
 }
-if (D == "true") {
-var DE = "0.50"
+if (D == true) {
+var DE = 0.50
 }else{
-var DE = "0"
+var DE = 0
 }
-if (E == "true") {
-var EE = "2"
+if (E == true) {
+var EE = 2
 }else{
-var EE = "0"
+var EE = 0
 }
-if (F == "true") {
-var FE = "1"
+if (F == true) {
+var FE = 1
 }else{
-var FE = "0"
+var FE = 0
 }
-if (G == "true") {
-var GE = "1.50"
+if (G == true) {
+var GE = 1.50
 }else{
-var GE = "0"
+var GE = 0
 }
-if (H == "true") {
-var HE = "2.50"
+if (H == true) {
+var HE = 2.50
 }else{
-var HE = "0"
+var HE = 0
 }
-if (I == "true") {
-var IE = "1"
+if (I == true) {
+var IE = 1
 }else{
-var IE = "0"
+var IE = 0
 }
-if (J == "true") {
-var JE = "1"
+if (J == true) {
+var JE = 1
 }else{
-var JE = "0"
+var JE = 0
 }
-if (K == "true") {
-var KE = "1"
+if (K == true) {
+var KE = 1
 }else{
-var KE = "0"
+var KE = 0
 }
-if (L == "true") {
-var LE = "1"
+if (L == true) {
+var LE = 1
 }else{
-var LE = "0"
+var LE = 0
 }
-if (M == "true") {
-var ME = "1"
+if (M == true) {
+var ME = 1
 }else{
-var ME = "0"
+var ME = 0
 }
-if (N == "true") {
-var NE = "1"
+if (N == true) {
+var NE = 1
 }else{
-var NE = "0"
+var NE = 0
 }
-if (O == "true") {
-var OE = "0.50"
+if (O == true) {
+var OE = 0.50
 }else{
-var OE = "0"
+var OE = 0
 }
-if (P == "true") {
-var PE = "1"
+if (P == true) {
+var PE = 1
 }else{
-var PE = "0"
+var PE = 0
 }
-if (Q == "true") {
-var QE = "1"
+if (Q == true) {
+var QE = 1
 }else{
-var QE = "0"
+var QE = 0
 }
-if (R == "true") {
-var RE = "1"
+if (R == true) {
+var RE = 1
 }else{
-var RE = "0"
+var RE = 0
 }
-if (S == "true") {
-var SE = "0.50"
+if (S == true) {
+var SE = 0.50
 }else{
-var SE = "0"
+var SE = 0
 }
-if (T == "true") {
-var TE = "1"
+if (T == true) {
+var TE = 1
 }else{
-var TE = "0"
+var TE = 0
 }
+     var TotalT = AE + BE + CE + DE + EE + FE + GE + HE + IE + JE + KE + LE + ME + NE + OE + PE + QE + RE + SE + TE
+    
+     console.log(TotalT)
 }
 
