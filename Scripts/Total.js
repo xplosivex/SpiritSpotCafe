20dc03ccf879f75397199aed8c0e772c04f08a3a
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-22 11:39:25 -0700

    Update Total.js

--- Scripts/Total.js
+++ Scripts/Total.js
@@ -162,9 +162,11 @@ var TE = 0
     if (FFF == "NDD") {
     var TotalG =  "Total: " + "$" + TotalK + " + Special Request"
     document.getElementById("TotalL").innerHTML = TotalG; 
+     document.getElementById("TotalB").innerHTML = TotalG;
      document.getElementById("TotalM").value = TotalG; 
     }else {
      document.getElementById("TotalL").innerHTML = TotalT; 
+     document.getElementById("TotalB").innerHTML = TotalG;
      document.getElementById("TotalM").value = TotalT; 
     }
 }
