d548ed96d249898b76dcb8cfaaca427ba8681e2c
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-22 08:53:22 -0700

    Create Total.js

---
+++ Scripts/Total.js
@@ -0,0 +1,22 @@
+setInterval(Total,0) 
+   function Total(){
+ var A =  document.getElementById("ExtraA").checked 
+ var B = document.getElementById("ExtraB").checked
+ var C = document.getElementById("ExtraC").checked
+ var E = document.getElementById("ExtraD").checked
+ var G = document.getElementById("ExtraF").checked
+ var H = document.getElementById("ExtraH").checked
+ var I = document.getElementById("ExtraI").checked
+ var J = document.getElementById("ExtraJ").checked
+ var K = document.getElementById("ExtraK").checked
+var L = document.getElementById("ExtraL").checked 
+var M = document.getElementById("ExtraM").checked 
+var N = document.getElementById("ExtraN").checked
+var O = document.getElementById("ExtraO").checked
+var P = document.getElementById("ExtraP").checked
+var Q = document.getElementById("ExtraQ").checked
+var R = document.getElementById("ExtraR").checked
+var S = document.getElementById("ExtraS").checked
+var T = document.getElementById("ExtraT").checked
+
+}
