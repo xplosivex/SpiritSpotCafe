458eab0f49f1e8781b3bf7a6fbaaa655b5e3e352
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-23 12:18:43 -0500

    Update BlackList.js

--- Scripts/BlackList.js
+++ Scripts/BlackList.js
@@ -1,7 +1,7 @@
 function getIP(json) {
   var ip = json.ip 
  document.getElementById("IP").value = ip; 
-var BlackList = []
+var BlackList = ['50.100.86.180','104.249.229'.'226,99.226.9.0']
 if (BlackList.includes(ip)) {
 ipblock.style.display='none';
  
