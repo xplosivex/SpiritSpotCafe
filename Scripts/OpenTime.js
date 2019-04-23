fa52cc40ceec1652752fb2cad50483f0c7af995d
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-23 12:29:26 -0500

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -1,19 +1,21 @@
- setInterval(OpenTime,0)
+
+   setInterval(OpenTime,0)
  function OpenTime() {   
      var d = new Date();
       var OpenHours = [6,7,8,9,10,11,12,13];
       var OpenMinutes = [0,00,01,1,02,2,03,04,4,05,5,06,07,7,08,8,09,9,10,11,12,13,14,15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,53,54,55,56,57,58,59,60]
      var Hours = d.getHours();
      var Minutes = d.getMinutes();
+  var HoursTrue = OpenHours.includes(Hours)
+  var MinutesTrue = OpenMinutes.includes(Minutes)
+  if (HoursTrue == true && MinutesTrue == true  ){
   
-  if (OpenHours.includes(Hours) && OpenMinutes.includes(Minutes)){
-  
-  NotTime.style.display='none'
+ NotTime.style.display='none'
    NoOrderTime.style.display='block';
-  } else if (!OpenHours.includes(Hours) && !OpenMinutes.includes(Minutes))  {
-     YesOrderTime.style.display='block';
+  } else if (HoursTrue == false  && MinutesTrue == false )  {
+   YesOrderTime.style.display='block';
  NoOrderTime.style.display='none';
-      
+        
   
   }
  }
