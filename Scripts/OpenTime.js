dba251c7f3266957cc6309ec5f66bdc994973529
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 11:11:08 -0500

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -5,9 +5,9 @@ function OpenTime () {
    var OpenHours = [6,7,8,9,10,11,12,13];
   var HTrue = OpenHours.includes(hours)
     if (HTrue = true) {
-    document.getElementById("NotTime").style.display = "none"
+    document.getElementById("NotTime").style.display = "block"
      document.getElementById("NoOrderTime").style.display = "none" 
-     document.getElementById("YesOrderTime").style.display = "block" 
+     document.getElementById("YesOrderTime").style.display = "none" 
     } else {
       document.getElementById("NotTime").style.display = "none"
      document.getElementById("NoOrderTime").style.display = "block" 
