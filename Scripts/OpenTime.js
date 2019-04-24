c5e52a72f9d65b24fbb00736d1d238e5188b979b
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 13:13:13 -0700

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -2,18 +2,20 @@ setInterval(OpenTime,0)
 function OpenTime () {
   var myDate = new Date();
   var hours = myDate.getHours();
- 
-    if ( hours > 6) {
-      if (hours < 15){
-    document.getElementById("NotTime").style.display = "block"
-     document.getElementById("NoOrderTime").style.display = "none" 
-     document.getElementById("YesOrderTime").style.display = "block" 
-      } else {
-      document.getElementById("NotTime").style.display = "none"
-     document.getElementById("NoOrderTime").style.display = "block" 
-     document.getElementById("YesOrderTime").style.display = "none" 
-    }
-      } 
-
- }
+ var days = myDate.getDay() 
+ if ( days == 0 || days == 6 ){
+document.getElementById("NotTime").style.display = "none"
+document.getElementById("NoOrderTime").style.display = "block" 
+document.getElementById("YesOrderTime").style.display = "none" 
+} else {
+if ( hours > 6) {
+  if (hours < 16){
+document.getElementById("NotTime").style.display = "block"
+document.getElementById("NoOrderTime").style.display = "none" 
+document.getElementById("YesOrderTime").style.display = "block" 
+} else {
+document.getElementById("NotTime").style.display = "none"
+document.getElementById("NoOrderTime").style.display = "block" 
+document.getElementById("YesOrderTime").style.display = "none" 
+}}}}
 
