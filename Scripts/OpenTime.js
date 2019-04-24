9c21cf1c03291c1027938235ebeadb74c25000ea
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 13:22:02 -0700

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -9,7 +9,7 @@ document.getElementById("NoOrderTime").style.display = "block"
 document.getElementById("YesOrderTime").style.display = "none" 
 } else {
 if ( hours > 6) {
-  if (hours < 15){
+  if (hours < 14){
 document.getElementById("NotTime").style.display = "block"
 document.getElementById("NoOrderTime").style.display = "none" 
 document.getElementById("YesOrderTime").style.display = "block" 
