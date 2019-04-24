f55ce35b14a477ab9e90e4acc1f5a88bdf4c641f
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 12:38:38 -0500

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -8,12 +8,12 @@ function OpenTime () {
     document.getElementById("NotTime").style.display = "block"
      document.getElementById("NoOrderTime").style.display = "none" 
      document.getElementById("YesOrderTime").style.display = "block" 
-      }
       } else {
       document.getElementById("NotTime").style.display = "none"
      document.getElementById("NoOrderTime").style.display = "block" 
      document.getElementById("YesOrderTime").style.display = "none" 
     }
+      } 
 
  }
 
