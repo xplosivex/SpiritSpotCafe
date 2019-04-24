34e99a6fe880d92fd1ded7aaf58bd595c9822527
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 11:35:10 -0500

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -2,7 +2,7 @@ setInterval(OpenTime,0)
 function OpenTime () {
   var myDate = new Date();
   var hours = myDate.getHours();
-   var OpenHours = [6,7,8,9,10,11,12,13];
+   var OpenHours = [12103];
   var HTrue = OpenHours.includes(hours)
     if (HTrue = true) {
     document.getElementById("NotTime").style.display = "block"
