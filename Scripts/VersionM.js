74fc3a4559042bfc2570f4554d179f96eed6477e
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 11:48:12 -0500

    Update VersionM.js

--- Scripts/VersionM.js
+++ Scripts/VersionM.js
@@ -1,6 +1,13 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
-var ver = Math.floor((Math.random() * 100) + 1);
+var man = new Date(); 
+var day = man.getDay()
+var month = man.getMonth()
+var year = man.getYear()
+var hour = man.getHours()
+var minute = man.getMinutes()
+var second = man.getSeconds()
+var ver = day + month + year + hour + minute + second
 var srcS = "https://www.spiritspotcafe.com/Scripts/OpenTime.js" + "?v=" + ver;
 script.src = srcS
 document.body.appendChild(script);
