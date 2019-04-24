2cd5e766e10f0aa824d941336bf9c41ec399cba2
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-24 13:07:09 -0700

    Update OpenTime.js

--- Scripts/OpenTime.js
+++ Scripts/OpenTime.js
@@ -4,7 +4,7 @@ function OpenTime () {
   var hours = myDate.getHours();
  
     if ( hours > 6) {
-      if (hours < 14){
+      if (hours < 16){
     document.getElementById("NotTime").style.display = "block"
      document.getElementById("NoOrderTime").style.display = "none" 
      document.getElementById("YesOrderTime").style.display = "block" 
