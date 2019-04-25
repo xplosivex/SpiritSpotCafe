28ec6d48811331d689e97b3f904ebe2c9f7431e8
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-25 10:29:07 -0500

    Update OpenTimeIndex.js

--- Scripts/OpenTimeIndex.js
+++ Scripts/OpenTimeIndex.js
@@ -5,8 +5,10 @@ var hours = myDate.getHours()
 var days = myDate.getDay() 
 var minutes = myDate.getMinutes()
 if (days == 0 || days == 6){
-var closeHour = 16 - hours
-var openHour = 14 - hours
+ x = 16
+  y = 13
+var closeHour =  x - hours
+var openHour = y - hours
 } else {
 if ( hours > 6) {
   if (hours < 15){
