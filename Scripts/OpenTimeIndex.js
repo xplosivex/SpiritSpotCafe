ca56373506d49395a3b3fbd8e081873c4f54f9e7
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-25 10:36:04 -0500

    Update OpenTimeIndex.js

--- Scripts/OpenTimeIndex.js
+++ Scripts/OpenTimeIndex.js
@@ -5,16 +5,16 @@ var hours = myDate.getHours()
 var days = myDate.getDay() 
 var minutes = myDate.getMinutes()
 if (days == 0 || days == 6){
- x = 16
-  y = 13
-var closeHour =  x - hours
-var openHour = y - hours
+var x = 16
+var  y = 13
+
+
 } else {
 if ( hours > 6) {
   if (hours < 15){
-document.getElementById("TimeUntil").innerHTML = "Spirit Spot Orders Will Close In " + openHour + " Hours" ;
+document.getElementById("TimeUntil").innerHTML = 15 - hours;
 } else {
-document.getElementById("TimeUntil").innerHTML = "Spirit Spot Orders Will Open In " + closeHour + " Hours" ;
+document.getElementById("TimeUntil").innerHTML = 15 - hours ;
 }
 }
 }
