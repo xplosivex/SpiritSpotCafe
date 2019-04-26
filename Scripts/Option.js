9acecd6c77ca5c45457cfcd7703ae596da9a1061
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-04-26 11:51:51 -0500

    Update Option.js

--- Scripts/Option.js
+++ Scripts/Option.js
@@ -9,19 +9,13 @@ var here = document.getElementById("Type").value
     else if ( here == "Hot Chocolate" || here == "None"  ){
    hiddenDiv.style.display='none';
      hiddenDiv3.style.display='none';
-     document.getElementById("ToppingsA").checked = false; 
-     document.getElementById("ToppingsB").checked = false;
-      document.getElementById("ToppingsC").checked = false; 
-     document.getElementById("ToppingsD").checked = false;
+   
       
     }  else if ( here == "Smoothie" ) {
      hiddenDiv3.style.display='block';
     Form.fileURL.focus();
      hiddenDiv.style.display='none';
-     document.getElementById("ToppingsA").checked = false; 
-     document.getElementById("ToppingsB").checked = false;
-      document.getElementById("ToppingsC").checked = false; 
-     document.getElementById("ToppingsD").checked = false;
+    
     }
 
  
@@ -37,26 +31,7 @@ function change2(){
     else {
     hiddenDiv2.style.display='none';
     }if ( here2 == "No") {
-  document.getElementById("ExtraA").checked = false; 
-document.getElementById("ExtraB").checked = false;
- document.getElementById("ExtraC").checked = false;
-document.getElementById("ExtraC").checked = false;
-document.getElementById("ExtraD").checked = false;
-document.getElementById("ExtraF").checked = false;
-document.getElementById("ExtraG").checked = false;
-document.getElementById("ExtraH").checked = false;
-document.getElementById("ExtraI").checked = false;
-document.getElementById("ExtraJ").checked = false;
-document.getElementById("ExtraK").checked = false;
-document.getElementById("ExtraL").checked  = false;
-document.getElementById("ExtraM").checked = false;
-document.getElementById("ExtraN").checked = false;
-document.getElementById("ExtraO").checked = false;
-document.getElementById("ExtraP").checked = false;
-document.getElementById("ExtraQ").checked = false;
-document.getElementById("ExtraR").checked = false;
-document.getElementById("ExtraS").checked = false;
-document.getElementById("ExtraT").checked = false;  
+ 
 
 }
 }
