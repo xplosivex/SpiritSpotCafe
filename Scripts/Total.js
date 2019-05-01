ad5d007d28b8e62c0b8b7fcdad1d2d2d5ca7fa8f
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-05-01 13:26:43 -0700

    Update Total.js

--- Scripts/Total.js
+++ Scripts/Total.js
@@ -499,6 +499,7 @@ store.set('Email', {E: document.getElementById("Email").value})
     console.log(store.get('Email').E)
     var TYPE = document.getElementById("Type").value
     store.set('drink', { drink:TYPE})
+    console.log(store.get('drink').drink) 
 if ( TYPE == "Hot Caramel"  || TYPE == "Hot Vanilla" || TYPE == "Iced Caramel" || TYPE  == "Iced Vanilla"  ) {
 store.set('ToppingA', {A: document.getElementById("ToppingsA").value})
 store.set('ToppingB', {B: document.getElementById("ToppingsB").value})
@@ -512,6 +513,9 @@ store.set('ToppingD', {D: document.getElementById("ToppingsD").value})
 store.set('ToppingS', {S: document.getElementById("ToppingsJ").value})
    console.log(store.get('ToppingS').S) 
 }
+    var Extra = document.getElementById("Extra2").value
+store.set('SnackC', { C: document.getElementById("Extra2").value})
+    if (Extra == "Yes") {
 store.set('Snack1', { one: document.getElementById("AQ").value})
 store.set('Snack2', { two: document.getElementById("BQ").value })
 store.set('Snack3', { three: document.getElementById("CQ").value })
@@ -535,8 +539,32 @@ store.set('Snack20', { twenty: document.getElementById("TQ").value })
 console.log(store.get('Snack1').one) 
 console.log(store.get('Snack2').two)
 console.log(store.get('Snack3').three)
-  }
-
-
+    }
+    }
+function Restore() {
+   if (store.get('SnackC').C == "Yes"){ 
+document.getElementById("Extra2").value = "Yes";
+ document.getElementById("AQ").value = store.get('Snack1').one;
+ document.getElementById("BQ").value = store.get('Snack2').two;
+document.getElementById("CQ").value = store.get('Snack3').three;
+document.getElementById("DQ").value = store.get('Snack4').four;
+document.getElementById("EQ").value = store.get('Snack5').five;
+document.getElementById("FQ").value = store.get('Snack6').six;
+document.getElementById("GQ").value = store.get('Snack7').seven;
+document.getElementById("HQ").value = store.get('Snack8').eight;
+document.getElementById("IQ").value = store.get('Snack9').nine;
+document.getElementById("JQ").value = store.get('Snack10').ten;
+document.getElementById("KQ").value = store.get('Snack11').four;
+document.getElementById("LQ").value = store.get('Snack12').tweleve;
+document.getElementById("MQ").value = store.get('Snack13').thirteen;
+document.getElementById("NQ").value = store.get('Snack14').fourteen;
+document.getElementById("OQ").value = store.get('Snack15').fifteen;
+document.getElementById("PQ").value = store.get('Snack16').sixteen; 
+document.getElementById("QQ").value = store.get('Snack17').seventeen;
+document.getElementById("RQ").value = store.get('Snack18').eightteen;
+document.getElementById("SQ").value = store.get('Snack19').nineteen;
+document.getElementById("TQ").value = store.get('Snack20').twenty;
+   }
+}
                         
    
