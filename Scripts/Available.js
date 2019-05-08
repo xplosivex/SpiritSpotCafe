5b5ee62a5f6a0f7ce589c9b0a4acb1a1c2572e38
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-05-08 12:26:57 -0500

    Update Available.js

--- Scripts/Available.js
+++ Scripts/Available.js
@@ -5,111 +5,111 @@ if (B1.includes("Available")){}else{
 BQ.style.display='none';
 }
 var B2 = document.getElementById("B2").innerHTML
-if (B2.includes("Available")){}else{
+if (B2.includes("Available") || B2.includes("available")){}else{
 EQ.style.display='none';
 }
 var B3 = document.getElementById("B3").innerHTML
-if (B3.includes("Available")){}else{
+if (B3.includes("Available") || B3.includes("available")){}else{
 GQ.style.display='none';
 }
 var B4 = document.getElementById("B4").innerHTML
-if (B4.includes("Available")){}else{
+if (B4.includes("Available") || B4.includes("available")){}else{
 HQ.style.display='none';
 }
 var B5 = document.getElementById("B5").innerHTML
-if (B5.includes("Available")){}else{
+if (B5.includes("Available") || B5.includes("available")){}else{
 IQ.style.display='none';
 }
 var B6 = document.getElementById("B6").innerHTML
-if (B6.includes("Available")){}else{
+if (B6.includes("Available") || B6.includes("available")){}else{
 Top1.style.display='none';
 }
 var B7 = document.getElementById("B7").innerHTML
-if (B7.includes("Available")){}else{
+if (B7.includes("Available") || B7.includes("available")){}else{
 Top2.style.display='none';
 }
 var B8 = document.getElementById("B8").innerHTML
-if (B8.includes("Available")){}else{
+if (B8.includes("Available") || B8.includes("available")){}else{
 Top3.style.display='none';
 }
 var B9 = document.getElementById("B9").innerHTML
-if (B9.includes("Available")){}else{
+if (B9.includes("Available") || B9.includes("available")){}else{
 Top4.style.display='none';
 }
 var B10 = document.getElementById("B10").innerHTML
-if (B10.includes("Available")){}else{
+if (B10.includes("Available") || B10.includes("available")){}else{
 T4.style.display='none';
 }
 var B11 = document.getElementById("B11").innerHTML
-if (B11.includes("Available")){}else{
+if (B11.includes("Available") || B11.includes("available")){}else{
 T3.style.display='none';
 }
 var B12 = document.getElementById("B12").innerHTML
-if (B12.includes("Available")){}else{
+if (B12.includes("Available") || B12.includes("available")){}else{
 T5.style.display='none';
 }
 var B13 = document.getElementById("B13").innerHTML
-if (B13.includes("Available")){}else{
+if (B13.includes("Available") || B13.includes("available")){}else{
 T6.style.display='none';
 }
 var B14 = document.getElementById("B14").innerHTML
-if (B14.includes("Available")){}else{
+if (B14.includes("Available") || B14.includes("available")){}else{
 FQ.style.display='none';
 }
 var B15 = document.getElementById("B15").innerHTML
-if (B15.includes("Available")){}else{
+if (B15.includes("Available") || B15.includes("available")){}else{
 JQ.style.display='none';
 }
 var B16 = document.getElementById("B16").innerHTML
-if (B16.includes("Available")){}else{
+if (B16.includes("Available") || B16.includes("available")){}else{
 LQ.style.display='none';
 }
 var B17 = document.getElementById("B17").innerHTML
-if (B17.includes("Available")){}else{
+if (B17.includes("Available") || B17.includes("available")){}else{
 CQ.style.display='none';
 }
 var B18 = document.getElementById("B18").innerHTML
-if (B18.includes("Available")){}else{
+if (B18.includes("Available") || B18.includes("available")){}else{
 KQ.style.display='none';
 }
 var B19 = document.getElementById("B19").innerHTML
-if (B19.includes("Available")){}else{
+if (B19.includes("Available") || B19.includes("available")){}else{
 MQ.style.display='none';
 }
 var B20 = document.getElementById("B20").innerHTML
-if (B20.includes("Available")){}else{
+if (B20.includes("Available") || B20.includes("available")){}else{
 NQ.style.display='none';
 }
 var B21 = document.getElementById("B21").innerHTML
-if (B21.includes("Available")){}else{
+if (B21.includes("Available") || B21.includes("available")){}else{
 OQ.style.display='none';
 }
 var B22 = document.getElementById("B22").innerHTML
-if (B22.includes("Available")){}else{
+if (B22.includes("Available") || B22.includes("available")){}else{
 PQ.style.display='none';
 }
 var B23 = document.getElementById("B23").innerHTML
-if (B23.includes("Available")){}else{
+if (B23.includes("Available") || B23.includes("available")){}else{
 DQ.style.display='none';
 }
 var B24 = document.getElementById("B24").innerHTML
-if (B24.includes("Available")){}else{
+if (B24.includes("Available") || B24.includes("available")){}else{
 AQ.style.display='none';
 }
 var B25 = document.getElementById("B25").innerHTML
-if (B25.includes("Available")){}else{
+if (B25.includes("Available") || B25.includes("available")){}else{
 QQ.style.display='none';
 }
 var B26 = document.getElementById("B26").innerHTML
-if (B26.includes("Available")){}else{
+if (B26.includes("Available") || B26.includes("available")){}else{
 RQ.style.display='none';
 }
 var B27 = document.getElementById("B27").innerHTML
-if (B27.includes("Available")){}else{
+if (B27.includes("Available") || B27.includes("available")){}else{
 SQ.style.display='none';
 }
 var B28 = document.getElementById("B28").innerHTML
-if (B28.includes("Available")){}else{
+if (B28.includes("Available") || B28.includes("available")){}else{
 TQ.style.display='none';
 }
 }
