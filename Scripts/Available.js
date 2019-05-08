716ce9908c3be7330dfe9c4c6d169caef2a9f25c
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-05-08 12:03:46 -0500

    Update Available.js

--- Scripts/Available.js
+++ Scripts/Available.js
@@ -1,12 +1,29 @@
-var B1 = document.getElementById("B1").innerHTML
+setTimeout(Aval,3000)
+	function Aval(){
+	var B1 = document.getElementById("B1").innerHTML
 if (B1.includes("Available")){}else{
 BQ.style.display='none';
 }
 var B2 = document.getElementById("B2").innerHTML
+if (B2.includes("Available")){}else{
+EQ.style.display='none';
+}
 var B3 = document.getElementById("B3").innerHTML
+if (B3.includes("Available")){}else{
+GQ.style.display='none';
+}
 var B4 = document.getElementById("B4").innerHTML
+if (B4.includes("Available")){}else{
+HQ.style.display='none';
+}
 var B5 = document.getElementById("B5").innerHTML
+if (B5.includes("Available")){}else{
+IQ.style.display='none';
+}
 var B6 = document.getElementById("B6").innerHTML
+if (B6.includes("Available")){}else{
+Top1.style.display='none';
+}
 var B7 = document.getElementById("B7").innerHTML
 var B8 = document.getElementById("B8").innerHTML
 var B9 = document.getElementById("B9").innerHTML
@@ -29,3 +46,4 @@ var B25 = document.getElementById("B25").innerHTML
 var B26 = document.getElementById("B26").innerHTML
 var B27 = document.getElementById("B27").innerHTML
 var B28 = document.getElementById("B28").innerHTML
+}
