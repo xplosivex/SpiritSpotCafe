b920ff42f895976fb0930b10fc3c022f6e60a478
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2019-05-09 14:50:56 -0500

    Create VersionL.js

---
+++ Scripts/VersionL.js
@@ -0,0 +1,15 @@
+function loadStyle(href, callback){
+    // avoid duplicates
+    for(var i = 0; i < document.styleSheets.length; i++){
+        if(document.styleSheets[i].href == href){
+            return;
+        }
+    }
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://www.spiritspotcafe.com/CSS/Table.css';
+    if (callback) { link.onload = function() { callback() } }
+    head.appendChild(link);
+}
